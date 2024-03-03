de190f4c7543c8fb1095202186d93e6eca425f2c
Prince Mishra <prince.pm.mishra@gmail.com>
2024-03-03 20:53:47 +0000

    Major Changes On the Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,71 +1,176 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function ArtWebsite() {
+// Header , Intro , Project , Contact, Footer 
+
+// Header
+
+import { Link } from "react-router-dom";
+import logo from "./assets/pmlogo.svg";
+import DeviceImage from "./assets/hero-devices.svg";
+import AvatarImage from "./assets/mf-avatar.svg";
+
+const Nav = () => {
   return (
-    <div className="bg-black text-white mx-auto min-h-screen">
-      <header className="flex justify-between items-center py-6 px-6 bg-gray-800">
-        <h1 className="text-4xl font-bold text-yellow-500"> Prince </h1>
-        <nav className="flex space-x-4">
-          {/* <a href="#about" className="text-gray-300 hover:text-yellow-500">Selected Work</a> */}
-          <a href="#about" className="text-gray-300 hover:text-yellow-500">About</a>
-          <a href="#contact" className="text-gray-300 hover:text-yellow-500">Contact</a>
-        </nav>
-      </header>
-
-      <main>
-        <section className="h-[500px] bg-cover bg-no-repeat bg-center" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1530305408560-82d13781b33a?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)' }}>
-          <div className="mx-auto px-6 flex flex-col items-center justify-center h-full">
-            <h1 className="text-6xl font-bold text-white mb-8"> Welcome</h1>
-            <h2 className="text-4xl font-bold text-white mb-4">DEV by</h2>
-            <h3 className="text-3xl font-bold text-white">PRINCE MISHRA</h3>
-          </div>
-        </section>
-
-        {/* <section className="about py-20 bg-gray-800">
-          <div className="container mx-auto px-6 space-y-10">
-            <p className="text-xl text-gray-300">2024 by PrinceMishra..</p>
-          </div>
-        </section> */}
-
-        <section className="contact py-20 bg-gray-900">
-          <div className="container mx-auto px-6">
-            <h2 className="text-3xl font-bold text-center text-yellow-500 mb-10">Contact</h2>
-            <form>
-              <div className="mb-6">
-                <label htmlFor="name" className="block text-lg font-medium text-gray-300 mb-2">Name</label>
-                <input type="text" id="name" className="w-full border border-gray-700 p-2 rounded-md shadow-sm bg-gray-800 text-white" />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="email" className="block text-lg font-medium text-gray-300 mb-2">Email</label>
-                <input type="email" id="email" className="w-full border border-gray-700 p-2 rounded-md shadow-sm bg-gray-800 text-white" />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="message" className="block text-lg font-medium text-gray-300 mb-2">Message</label>
-                <textarea id="message" rows="5" className="w-full border border-gray-700 p-2 rounded-md shadow-sm bg-gray-800 text-white"></textarea>
-              </div>
-              <button type="submit" className="inline-flex items-center px-3 py-2 font-medium rounded-md text-center text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none">Send</button>
-            </form>
-          </div>
-        </section>
-      </main>
-
-      <footer className="text-center py-4 bg-gray-800">
-        <p className="text-gray-300">Copyright &copy; 2024 Prince Mishra. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+    <nav className="flex justify-between px-32 pt-10 items-center">
+      {/* <Link to="/"> */}
+        <img src={logo} alt="Logo" width={60} height={60} />
+      {/* </Link>   */}
+      <ul className="flex space-x-7 items-center justify-center">
+        <li className="text-lg"> Projects </li>
+        <li> 
+          <button className="border-[#5c1be0] border-2 rounded-full px-6 py-2 text-lg text-[#5c1be0]"> Say Hello </button>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
+const Header = () => {
+  return (
+    <div>
+      <Nav />
+      <section className="flex justify-center items-center">
+        <div className="flex flex-col justify-center w-2/3 items-center  pt-32">
+          <div className="flex flex-col items-center justify-center" >
+            <h1 className="text-5xl font-bold">Hey, I am Prince, A Full Stack Developer </h1>
+            <p className="text-2xl mt-5"> I code beautifully simple things, and I love what I do. </p>
+            {/* <button className="border-[#5c1be0] border-2 rounded-full px-6 py-2 text-lg text-[#5c1be0] mt-5"> Get in Touch </button> */}
+          </div>
+          <div className="flex flex-col justify-center items-center  pt-14 space-y-14">
+            <img src={AvatarImage}  alt="Avatar" />
+            <img src={DeviceImage} className="w-2/3" alt="Devices" />
+          </div>
+        </div>
+      </section>
 
-function App() {
+    </div>
+  )
+}
+
+// skill card 
+
+const SkillCard = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center">
+      <div className="flex items-center justify-center space-y-5 mt-[-130px]  border rounded-lg bg-white w-3/4">
+        {/* frontend skill */}
+        <div className="flex flex-col items-center justify-start pt-12 w-1/3 py-4 px-8 border-r h-full">
+          <div className="bg-[#84e6bc] p-6 rounded-full">
+            <svg className="h-12 svg-inline--fa fa-square-terminal fa-stack-1x has-text-dark" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="square-terminal" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M384 64c17.7 0 32 14.3 32 32V416c0 17.7-14.3 32-32 32H64c-17.7 0-32-14.3-32-32V96c0-17.7 14.3-32 32-32H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM83.9 149.6c-5.8 6.7-5 16.8 1.7 22.6L183.4 256 85.6 339.9c-6.7 5.8-7.5 15.8-1.7 22.6s15.9 7.5 22.6 1.7l112-96c3.5-3 5.6-7.5 5.6-12.1s-2-9.1-5.6-12.1l-112-96c-6.7-5.8-16.8-5-22.6 1.7zM208 352c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16s-7.2-16-16-16H208z"></path></svg>
+          </div>
+          <h1 className="text-2xl font-bold py-8"> Frontend Developer </h1>
+          <p >I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+          <div className="py-8">
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Languages: </h1>
+            <p>HTML, CSS, Javascript, ReactJs </p>
+          </div>
+          <div>
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Tools: </h1>
+            <ul className="flex flex-col space-y-1">
+              <li>VS Code</li>
+              <li>Codepen</li>
+              <li>GitHub</li>
+              <li>Netlify</li>
+            </ul>
+          </div>
+
+        </div>
+        {/* backend skill */}
+        <div className="flex flex-col items-center justify-start pt-12 w-1/3 py-4 px-8 border-r h-full">
+          <div className="bg-[#84e6bc] p-6 rounded-full">
+            <svg className="h-12 svg-inline--fa fa-square-terminal fa-stack-1x has-text-dark" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="square-terminal" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M384 64c17.7 0 32 14.3 32 32V416c0 17.7-14.3 32-32 32H64c-17.7 0-32-14.3-32-32V96c0-17.7 14.3-32 32-32H384zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM83.9 149.6c-5.8 6.7-5 16.8 1.7 22.6L183.4 256 85.6 339.9c-6.7 5.8-7.5 15.8-1.7 22.6s15.9 7.5 22.6 1.7l112-96c3.5-3 5.6-7.5 5.6-12.1s-2-9.1-5.6-12.1l-112-96c-6.7-5.8-16.8-5-22.6 1.7zM208 352c-8.8 0-16 7.2-16 16s7.2 16 16 16H368c8.8 0 16-7.2 16-16s-7.2-16-16-16H208z"></path></svg>
+          </div>
+          <h1 className="text-2xl font-bold py-8"> Backend Developer </h1>
+          <p >I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+          <div className="py-8">
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Languages: </h1>
+            <p>NodeJs, ExpressJs, MongoDB, Firebase </p>
+          </div>
+          <div>
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Tools: </h1>
+            <ul className="flex flex-col space-y-1">
+              <li>VS Code</li>
+              <li>Postman</li>
+              <li>GitHub</li>
+              <li>Heroku</li>
+            </ul>
+          </div>
+        </div>
+        {/* DSA and CP skill */}
+        <div className="flex flex-col items-center justify-start pt-12 w-1/3 py-4 px-8 h-full">
+          <div className="bg-[#84e6bc] p-6 rounded-full">
+            <svg className="h-12 svg-inline--fa fa-square-terminal fa-stack-1x has-text-dark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g id="Layer_33" data-name="Layer 33"><path d="M23,14a9.01,9.01,0,0,0-8.932,7.941,35.09,35.09,0,0,1-8.849-.6,1,1,0,1,0-.438,1.951A34.219,34.219,0,0,0,12,24c.693,0,1.375-.023,2.05-.057a8.979,8.979,0,0,0,2.918,5.717A35.505,35.505,0,0,1,12,30c-6.046,0-9.728-1.329-10-2V14.384c.054.026.12.046.175.071q.341.155.718.293c.1.037.206.073.312.108q.41.135.849.252c.072.019.14.041.214.059.367.092.747.175,1.135.25.1.02.209.037.313.056q.453.081.915.149c.121.017.24.035.361.051.358.047.715.09,1.073.126l.144.016c.407.04.809.071,1.206.1l.323.018c.3.018.591.032.878.042l.32.011c.368.01.727.017,1.064.017,1.046,0,2.081-.039,3.077-.115a1,1,0,0,0-.154-1.994C13.979,13.963,13,14,12,14c-6.046,0-9.728-1.329-10-2V6.384c.054.026.12.046.175.071q.341.154.718.293c.1.037.206.073.312.108q.41.135.849.252c.072.019.14.041.214.059.367.092.747.175,1.135.25.1.02.209.037.313.056q.453.081.915.149c.121.017.24.035.361.051.358.047.715.09,1.073.126l.144.016c.407.04.809.071,1.206.1l.323.018c.3.018.591.032.878.042l.32.011C11.3,7.993,11.663,8,12,8a34.219,34.219,0,0,0,7.219-.709,1,1,0,1,0-.438-1.952A32.123,32.123,0,0,1,12,6c-.515,0-1.007-.013-1.487-.031C5.4,5.777,2.305,4.634,2.011,4.016,2.364,3.3,6.025,2,12,2c6.044,0,9.727,1.329,10,2v7a1,1,0,0,0,2,0V4c0-3.157-7.543-4-12-4S0,.843,0,4V28c0,3.157,7.543,4,12,4a34.219,34.219,0,0,0,7.219-.709.9.9,0,0,0,.145-.065A9,9,0,1,0,23,14Zm0,16a7,7,0,1,1,7-7A7.008,7.008,0,0,1,23,30Z"/><path d="M26,18a.987.987,0,0,0-.85.5,5.1,5.1,0,0,0-5.457.746,1,1,0,0,0,1.323,1.5,3.069,3.069,0,0,1,2.616-.676A1,1,0,0,0,24,22h2a1,1,0,0,0,1-1V19A1,1,0,0,0,26,18Z"/><path d="M24.984,25.25a3.07,3.07,0,0,1-2.615.676A1,1,0,0,0,22,24H20a1,1,0,0,0-1,1v2a1,1,0,0,0,1,1,.986.986,0,0,0,.846-.5,4.961,4.961,0,0,0,5.461-.751,1,1,0,1,0-1.323-1.5Z"/><circle cx="5" cy="10" r="1"/><circle cx="5" cy="18" r="1"/><circle cx="5" cy="26" r="1"/></g></svg>
+          </div> 
+          <h1 className="text-2xl font-bold py-8"> DSA and CP </h1>
+          <p >I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+          <div className="py-8">
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Languages: </h1>
+            <p>C++ </p>
+          </div>
+          <div>
+            <h1 className="text-xl font-bold text-[#5c1be0] pb-2"> Tools: </h1>
+            <ul className="flex flex-col space-y-1">
+              <li>VS Code</li>
+              <li>Codeforces</li>
+              <li>Leetcode</li>
+              <li>GeeksforGeeks</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// Intro
+
+
+const Intro = () => {
   return (
     <>
-      <ArtWebsite />
+      <div className="bg-[#5c1be0] flex justify-center">
+        <div className="flex flex-col justify-center items-center w-1/2 pt-32 pb-60">
+          <h1 className="text-3xl text-white font-bold text-center mb-2">Hi, I'm Prince Mishra. Nice to meet you.</h1>
+          <p className="text-white p-2 text-center">
+            {/* Since beginning my journey as a freelance designer 12 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops. */}
+            Hi I am making websites since 2019. I have worked on many projects and have learned a lot. I am a full stack developer and I love to code. I am a self taught developer and I have learned a lot from the internet. I have worked on many projects and have learned a lot. I am a full stack developer and I love to code.
+          </p>
+        </div>
+      </div>
+      <SkillCard />
     </>
   )
 }
 
-export default App
+// Project
+const Project = () => {
+  return (
+    <div>Project</div>
+  )
+}
+
+// Contact
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+// Footer
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Intro />
+      <Project />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
